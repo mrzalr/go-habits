e402527c16f2e8f1f6bf922d25ac8f5a63008262
rizal.rusdiansyah <rizal.rusdiansyah@runsystem.id>
2023-10-24 10:37:52 +0700

    FIX weekRange - reset the time to 00:00

--- pkg/date/week_range.go
+++ pkg/date/week_range.go
@@ -7,8 +7,12 @@ import (
 func GetWeekRange(weekday int) WeekRange {
 	dayDuration := time.Hour * 24
 
-	// Here I use standard, week starts Monday and ends Sunday.
+	// The standard week is starts from Monday 00:00 until Sunday 24:00
 	start := time.Now().Add(-dayDuration * time.Duration(weekday-1))
-	end := time.Now().Add(dayDuration * time.Duration(7-weekday))
-	return WeekRange{start, end}
+	end := time.Now().Add(dayDuration * time.Duration(8-weekday))
+	return WeekRange{resetTime(start), resetTime(end)}
+}
+
+func resetTime(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
 }
